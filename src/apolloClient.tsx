41ba6f3af4fe497cfd92aa47fa73d92946f8e31e
Wesley Erasmus <wesley.erasmus119@gmail.com>
2024-10-19 23:45:51 +0200

    fixed connection to API

--- src/apolloClient.tsx
+++ src/apolloClient.tsx
@@ -1,22 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
-
-// const HASURA_ENDPOINT = import.meta.env.VITE_HASURA_ENDPOINT;
-// const HASURA_GRAPHQL_ADMIN_SECRET = import.meta.env.VITE_HASURA_ADMIN_SECRET;
-
-// const httpLink = new HttpLink({
-//   uri: HASURA_ENDPOINT,
-//   headers: {
-//     "x-hasura-admin-secret": HASURA_GRAPHQL_ADMIN_SECRET,
-//   },
-// });
-
-// const client = new ApolloClient({
-//   link: httpLink,
-//   cache: new InMemoryCache(),
-// });
-
+const API_ENDPOINT =
+  import.meta.env.VITE_API_ENDPOINT || "http://localhost:4000/";
 
 const httpLink = new HttpLink({
   uri: API_ENDPOINT,
