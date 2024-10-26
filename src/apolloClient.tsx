bd9f73e8acf234a83a057bd982c814cacd0423bd
Wesley Erasmus <wesley.erasmus119@gmail.com>
2024-10-26 13:32:21 +0200

    Update apolloClient.tsx

--- src/apolloClient.tsx
+++ src/apolloClient.tsx
@@ -1,13 +1,10 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-// const API_ENDPOINT =
-//   import.meta.env.VITE_API_ENDPOINT || "http://localhost:4000/";
 const API_ENDPOINT =
-  import.meta.env.VITE_API_ENDPOINT;
+  import.meta.env.VITE_API_ENDPOINT || "http://localhost:4000/";
 
 const httpLink = new HttpLink({
   uri: API_ENDPOINT,
-  // uri: "http://localhost:4000/",
 });
 
 const client = new ApolloClient({
